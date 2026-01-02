112d998f9916734efe7ec1b99a4c6107cf5d2af6
Danilo <danilosantossilveira.com@gmail.com>
2026-01-02 09:39:27 -0300

    Add files via upload

--- POO-Sabor-Express/app.py
+++ POO-Sabor-Express/app.py
@@ -1,23 +1,28 @@
 class Restaurante:
     restaurantes = []
     def __init__ (self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f' Nome: {self.nome}\n Categoria: {self.categoria}\n Estado: {self.ativo}\n'
-
-    def listar_restaurante():
-        print('Nome'.ljust(20),'|Categoria'.ljust(22),'|Ativo')
+        return f' Nome: {self._nome}\n Categoria: {self.categoria}\n Estado: {self.ativo}\n'
+    @classmethod
+    def listar_restaurante(cls):
+        print('Nome'.ljust(20),'|Categoria'.ljust(22),'|Estado')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo}')
 
+    @property
+    def ativo(self):
+        return 'ativado' if self._ativo else 'desativado'
     
-
+    def alterar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_1 = Restaurante('Rafa Sushi', 'Sushi')
+restaurante_1.alterar_estado()
 restaurante_2 = Restaurante('Alemão', 'Pizzaria')
 restaurante_3 = Restaurante('MD Lanches', 'Xis')
 
